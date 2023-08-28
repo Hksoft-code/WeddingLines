1bacb02ed104aa1d6bdf5764237e1bd292929743
NashibJr <nashibjr@gmail.com>
2023-08-28 07:04:26 -0700

    I have changed the first section as asked by Madam

--- components/Welcome.js
+++ components/Welcome.js
@@ -4,53 +4,20 @@ import React from "react";
 import Input from "./Input";
 
 const Welcome = () => {
-  const [state, setState] = React.useState({ venues: "", places: "" });
-  const handleChange = (event) =>
-    setState({ ...state, [event.target.name]: event.target.value });
+  const [place, setPlace] = React.useState("");
+
   return (
-    <div className="w-full flex border-b md:h-[450px] md:flex-row flex-col-reverse">
-      <div className="md:w-1/2 md:p-7 my-auto flex flex-col flex-wrap md:p-6 p-2 pt-6">
-        <p className="font-bold md:text-4xl mb-7 text-3xl">
-          Discover everything you need
-          <br className="max-md:hidden" /> to plan your big day
-        </p>
-        <p>
-          Search over 73,000 local professionals with reviews,
-          <br />
-          pricing, and more
-        </p>
-        <div className="mt-6 hidden lg:block">
-          <Input
-            type="text"
-            placeholder="Search for Eg.(Wedding Venues, Hotels)"
-            _class="p-3 md:w-1/3 w-1/4 outline-none border-2 rounded-md shadow-md mr-2"
-            handleChange={handleChange}
-            value={state.venues}
-            name_="venues"
-          />
-          <Input
-            type="text"
-            placeholder="in Eg.(Delhi, Nicobar)"
-            _class="p-3 md:w-1/3 w-1/4 outline-none border-2 rounded-md shadow-md mr-2"
-            handleChange={handleChange}
-            value={state.places}
-            name_="places"
-          />
-          <Input
-            type="button"
-            _class="p-3 w-1/4 outline-none border-2 rounded-md shadow-md mr-2 bg-red-500 text-white cursor-pointer hover:bg-red-400"
-            value="Find"
-          />
-        </div>
-        <div className="lg:hidden">
-          <Input
-            type="button"
-            value="Search vendors"
-            _class="p-3 w-full text-white bg-red-500 font-semibold mt-5 mb-4 rounded-md cursor-pointer"
-          />
-        </div>
-      </div>
-      <div className="md:w-1/2 md:h-auto flex justify-end items-end homepage h-[40vh]"></div>
+    <div className="w-full md:p-10 pt-7 pr-3 pl-3 pb-7 md:h-[65vh] h-[50vh] homepage flex flex-col justify-center items-center">
+      <h1 className="font-semibold text-4xl md:text-red-700 mb-5 text-center text-black">
+        The Wedding Line
+      </h1>
+      <Input
+        type="text"
+        value={place}
+        placeholder="Search for venues, hotels, party places etc or regions e.g Nicobar, Chamba"
+        handleChange={(event) => setPlace(event.target.value)}
+        _class="md:w-3/4 w-full md:p-4 p-3 outline-none rounded-lg"
+      />
     </div>
   );
 };
